53cbf55757f77fc66248a3aff8b6173fefd63ba1
Praise Oyedele <opra.temmy@gmail.com>
2025-10-08 03:05:35 +0100

    initial commit: first project

---
+++ intro.js
@@ -0,0 +1,5 @@
+const full_name = "Praise Oyedele";
+const age = "25";
+const prog_lang = "Javascript";
+
+console.log("Hello, my name is", full_name,", I am", age, "years old and my favourite programming language is", prog_lang);
